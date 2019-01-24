78e75331c34e4f9d8eb5adf58bad822e2e80f403
Nicolas fernandes <nicolas.fernandes.etu@univ-lille.fr>
2019-01-24 15:44:36 +0100

    jk

--- webserver/socket.c
+++ webserver/socket.c
@@ -3,6 +3,21 @@
 
 int creer_serveur(int port){
 
-	int socket(int domain, int type, int protocol);
+	//s descripteur du socket serveur
+	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_serveur == -1)
+	{
+		/* traitement de l’erreur */
+		perror("socket_serveur");
+	}
+
+	//Attachement de la socket serveur sur toutes les interfaces
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET; /* Socket ipv4 */
+	saddr.sin_port = htons(8080); /* Port d’écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+
+	//rattachement de la socket a une adresse et un port
+	bind(socket_serveur, const struct sockaddr *addr, socklen_t addrlen);
 
 }
