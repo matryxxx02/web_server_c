d64c40a77ab01fbd1e940e556f7cd58514582ab4
Nicolas fernandes <nicolas.fernandes.etu@univ-lille.fr>
2019-01-24 16:10:04 +0100

    ajout de listen

--- webserver/socket.c
+++ webserver/socket.c
@@ -24,4 +24,28 @@ int creer_serveur(int port){
 		/* traitement de l’erreur */
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	int socket_client;
+	socket_client = accept(socket_serveur, NULL, NULL);
+	if (socket_client == -1){
+		perror("accept");
+		/* traitement d’erreur */
+	}
+
+	/* On peut maintenant dialoguer avec le client */
+	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+	write(socket_client , message_bienvenue , strlen(message_bienvenue));
+
 }
