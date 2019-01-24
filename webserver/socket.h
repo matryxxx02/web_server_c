cdfa6f54f9f0a647177798d46565456d55dbb1a5
Pereira Nicolas <nicolas_pereira27@hotmail.com>
2019-01-24 11:53:37 +0100

    retour char

--- webserver/socket.h
+++ webserver/socket.h
@@ -3,4 +3,5 @@
 /** Crée une socket serveur qui écoute sur toute les interfaces IPv4
 de la machine sur le port passé en paramètre. La socket retournée doit pouvoir être utilisée directement par un appel à accept.
 La fonction retourne -1 en cas d’erreur ou le descripteur de la socket créée. */
-int creer_serveur(int port); #endif
+int creer_serveur(int port);
+#endif
