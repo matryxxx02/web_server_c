333dab7b9483ea5d7d68981c07da1f44f6d4a42d
Pereira Nicolas <nicolas_pereira27@hotmail.com>
2019-01-24 16:00:48 +0100

    indent

--- webserver/socket.c
+++ webserver/socket.c
@@ -3,25 +3,25 @@
 
 int creer_serveur(int port){
 
-	//s descripteur du socket serveur
-	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_serveur == -1){
-		/* traitement de l’erreur */
-		perror("socket_serveur");
-	}
+//s descripteur du socket serveur
+int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+if (socket_serveur == -1){
+	/* traitement de l’erreur */
+	perror("socket_serveur");
+}
 
-	//Attachement de la socket serveur sur toutes les interfaces
-		struct sockaddr_in saddr{
-		saddr.sin_family = AF_INET; /* Socket ipv4 */
-		saddr.sin_port = htons(8080); /* Port d’écoute */
-		saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
-	}
+//Attachement de la socket serveur sur toutes les interfaces
+	struct sockaddr_in saddr{
+	saddr.sin_family = AF_INET; /* Socket ipv4 */
+	saddr.sin_port = htons(8080); /* Port d’écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+}
 
 
-	//rattachement de la socket a une adresse et un port
-	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
-		perror("bind socker_serveur");
-		/* traitement de l’erreur */
-	}
+//rattachement de la socket a une adresse et un port
+if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
+	perror("bind socker_serveur");
+	/* traitement de l’erreur */
+}
 
 }
