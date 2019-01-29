ef239bc597a6a9c6587440e94c120b6e29768827
Pereira Nicolas <nicolas_pereira27@hotmail.com>
2019-01-29 10:25:09 +0100

    Main

--- webserver/main.c
+++ webserver/main.c
@@ -1,7 +1,9 @@
+# include "socket.h"
 # include <stdio.h>
 # include <string.h>
 
 int main ( int argc , char ** argv ) {
+	creer_serveur(8080);
 	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
 	if ( argc > 1 && strcmp ( argv [1] , " - advice " ) == 0) {
 		printf("Don ’t Panic !\n");
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 int creer_serveur(int port){
 	//On récupére le descripteur du socket serveur
@@ -14,14 +14,14 @@ int creer_serveur(int port){
 	}
 
 	struct sockaddr_in {
-		sa_family_t			sin_family; /* address family : AF_INET */
-		in_port_t				sin_port; 	/* port in network byte order */
+		sa_family_t		sin_family; /* address family : AF_INET */
+		in_port_t		sin_port; 	/* port in network byte order */
 		struct in_addr 	sin_addr; 	/* internet address */
 	};
 
 	/* Internet address . */
 	struct in_addr {
-		uint32_t 			s_addr;		/* address in network byte order */
+		uint32_t 		s_addr;		/* address in network byte order */
 	};
 
 	//Attachement de la socket serveur sur toutes les interfaces
