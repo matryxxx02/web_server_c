5bd6c9437fc1786d8f102b1af1ddb3961ff6df65
Nicolas fernandes <nicolas.fernandes.etu@univ-lille.fr>
2019-01-31 11:41:08 +0100

    resolution du probleme traitement d'un client a la fois

--- webserver/main.c
+++ webserver/main.c
@@ -27,29 +27,34 @@ int main ( int argc , char ** argv ) {
 
 	initialiser_signaux();
 
-	int idx = 0;
-
-	while(idx<5){
-		idx++;
-		int socket_client = accept_(socket_server);		
-		/* On peut maintenant dialoguer avec le client */
-		const char * message_bienvenue = "Bonjour,\n bienvenue sur notre serveur en construction !!\n";
-		write(socket_client , message_bienvenue , strlen(message_bienvenue));
-		sleep(1);
-		const char * message_bienvenue1 = "C'est un projet pour le S4 !!\n C'est le debut du projet \n";
-		write(socket_client , message_bienvenue1 , strlen(message_bienvenue1));
-		sleep(1);
-		const char * message_bienvenue2 = "Il nous faut 10 Lignes!!\n mais malheureusement je ne sais pas quoi dire\n";
-		write(socket_client , message_bienvenue2 , strlen(message_bienvenue2));
-		sleep(1);
-		const char * message_bienvenue3 = "De grosse modif sont attendu\n Revenez plus tard \n Je m'occupe de l'echo: \n";
-		write(socket_client , message_bienvenue3 , strlen(message_bienvenue3));
-
-		//pour la fonction echo plus tard :
+	while(1){
 		int s;
 		char buf[BLOCK_SIZE];
-		while((s=read(socket_client,&buf,BLOCK_SIZE))>0){
-			write(socket_client,&buf,s);
+		int socket_client = accept_(socket_server);		
+
+		int pid = fork();
+		if(pid==0){
+
+			/* On peut maintenant dialoguer avec le client */
+			const char * message_bienvenue = "Bonjour,\n bienvenue sur notre serveur en construction !!\n";
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			const char * message_bienvenue1 = "C'est un projet pour le S4 !!\n C'est le debut du projet \n";
+			write(socket_client , message_bienvenue1 , strlen(message_bienvenue1));
+			sleep(1);
+			const char * message_bienvenue2 = "Il nous faut 10 Lignes!!\n mais malheureusement je ne sais pas quoi dire\n";
+			write(socket_client , message_bienvenue2 , strlen(message_bienvenue2));
+			sleep(1);
+			const char * message_bienvenue3 = "De grosse modif sont attendu\n Revenez plus tard \n Je m'occupe de l'echo: \n";
+			write(socket_client , message_bienvenue3 , strlen(message_bienvenue3));
+
+			//fonction echo avec le client (renvoi ce que le client ecrit)
+			while((s=read(socket_client,&buf,BLOCK_SIZE))>0){
+				write(socket_client,&buf,s);
+			}
+
+		}else{
+			close(socket_client);
 		}
 		
 	}
