b1c4ce822c8e5b20a58d7c825d981da52c24d792
Pereira Nicolas <nicolas_pereira27@hotmail.com>
2019-02-07 11:55:38 +0100

    refactoring

--- webserver/main.c
+++ webserver/main.c
@@ -27,6 +27,20 @@ void initialiser_signaux(void){
 	}
 }
 
+void messageBienvenu (int socket_client) {
+	const char * message_bienvenue = "Bonjour,\n bienvenue sur notre serveur en construction !!\n";
+	write(socket_client , message_bienvenue , strlen(message_bienvenue));
+	
+	const char * message_bienvenue1 = "C'est un projet pour le S4 !!\n C'est le debut du projet \n";
+	write(socket_client , message_bienvenue1 , strlen(message_bienvenue1));
+	
+	const char * message_bienvenue2 = "Il nous faut 10 Lignes!!\n mais malheureusement je ne sais pas quoi dire\n";
+	write(socket_client , message_bienvenue2 , strlen(message_bienvenue2));
+	
+	const char * message_bienvenue3 = "De grosse modif sont attendu\n Revenez plus tard \n Je m'occupe de l'echo: \n";
+	write(socket_client , message_bienvenue3 , strlen(message_bienvenue3));
+}
+
 int main ( int argc , char ** argv ) {
 
 	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
@@ -51,30 +65,17 @@ int main ( int argc , char ** argv ) {
 		if(pid==0){
 			//dans le fils
 
-			/* On peut maintenant dialoguer avec le client */
-			const char * message_bienvenue = "Bonjour,\n bienvenue sur notre serveur en construction !!\n";
-			write(socket_client , message_bienvenue , strlen(message_bienvenue));
-			
-			const char * message_bienvenue1 = "C'est un projet pour le S4 !!\n C'est le debut du projet \n";
-			write(socket_client , message_bienvenue1 , strlen(message_bienvenue1));
-			
-			const char * message_bienvenue2 = "Il nous faut 10 Lignes!!\n mais malheureusement je ne sais pas quoi dire\n";
-			write(socket_client , message_bienvenue2 , strlen(message_bienvenue2));
-			
-			const char * message_bienvenue3 = "De grosse modif sont attendu\n Revenez plus tard \n Je m'occupe de l'echo: \n";
-			write(socket_client , message_bienvenue3 , strlen(message_bienvenue3));
-
 			char * nom = "Serveur : ";
 			FILE * file = fdopen(socket_client,"w+");
 			if(strcmp(fgets(buf,BLOCK_SIZE,file), "GET / HTTP/1.1\r\n") == 0){
 				while(strcmp(fgets(buf,BLOCK_SIZE,file), "\r\n") != 0){
 					fprintf(file,"%s%s",nom,buf);
 				}
-			} else {
-				char * reponse = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Lenght: 17\r\n\r\n400 Bad Request\r\n";
-				fprintf(file,"%s%s",nom,reponse);
+				messageBienvenu(socket_client);
+			}else {
+			char * reponse = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Lenght: 17\r\n\r\n400 Bad Request\r\n";
+			fprintf(file,"%s%s",nom,reponse);
 			}
-			printf("fin\n");
 			exit(0);
 
 		}else{
